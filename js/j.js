57401f10fccf00fd4cbab35fa658728fa19b0406
Daniel Dafoe <dannydafoe@gmail.com>
2016-09-05 21:41:45 -0400

    Add JS for Emojify functionality

---
+++ js/j.js
@@ -0,0 +1,314 @@
+var ed = {
+    data:
+    [
+        {
+            "n": "grin",
+            "un": "\u{1F601}"
+        },
+        {
+            "n": "tears",
+            "un": "\u{1F602}"
+        },
+        {
+            "n": "smile",
+            "un": "\u{1F60A}"
+        },
+        {
+            "n": "sweat",
+            "un": "\u{1F605}"
+        },
+        {
+            "n": "laugh",
+            "un": "\u{1F606}"
+        },
+        {
+            "n": "wink",
+            "un": "\u{1F609}"
+        },
+        {
+            "n": "tongue",
+            "un": "\u{1F60B}"
+        },
+        {
+            "n": "love",
+            "un": "\u{1F60D}"
+        },
+        {
+            "n": "pensive",
+            "un": "\u{1F614}"
+        },
+        {
+            "n": "unused",
+            "un": "\u{1F612}"
+        },
+        {
+            "n": "kiss",
+            "un": "\u{1F61A}"
+        },
+        {
+            "n": "disappointed",
+            "un": "\u{1F61E}"
+        },
+        {
+            "n": "angry",
+            "un": "\u{1F620}"
+        },
+        {
+            "n": "very angry",
+            "un": "\u{1F621}"
+        },
+        {
+            "n": "cry",
+            "un": "\u{1F622}"
+        },
+        {
+            "n": "fearful",
+            "un": "\u{1F628}"
+        },
+        {
+            "n": "weary",
+            "un": "\u{1F61A}"
+        },
+        {
+            "n": "scream",
+            "un": "\u{1F631}"
+        },
+        {
+            "n": "sleepy",
+            "un": "\u{1F62A}"
+        },
+        {
+            "n": "astonished",
+            "un": "\u{1F632}"
+        },
+        {
+            "n": "flushed",
+            "un": "\u{1F633}"
+        },
+        {
+            "n": "dizzy",
+            "un": "\u{1F635}"
+        },
+        {
+            "n": "rocket",
+            "un": "\u{1F680}"
+        },
+        {
+            "n": "metro",
+            "un": "\u{1F687}"
+        },
+        {
+            "n": "station",
+            "un": "\u{1F689}"
+        },
+        {
+            "n": "bus",
+            "un": "\u{1F68C}"
+        },
+        {
+            "n": "ambulance",
+            "un": "\u{1F691}"
+        },
+        {
+            "n": "fire engine",
+            "un": "\u{1F692}"
+        },
+        {
+            "n": "police",
+            "un": "\u{1F693}"
+        },
+        {
+            "n": "taxi",
+            "un": "\u{1F695}"
+        },
+        {
+            "n": "automobile",
+            "un": "\u{1F697}"
+        },
+        {
+            "n": "toilet",
+            "un": "\u{1F6BD}"
+        },
+        {
+            "n": "bath",
+            "un": "\u{1F6C0}"
+        },
+        {
+            "n": "snowman",
+            "un": "\u{26C4}"
+        },
+        {
+            "n": "ok",
+            "un": "\u{1F44C}"
+        },
+        {
+            "n": "snail",
+            "un": "\u{1F40C}"
+        },
+        {
+            "n": "snake",
+            "un": "\u{1F40D}"
+        },
+        {
+            "n": "horse",
+            "un": "\u{1F434}"
+        },
+        {
+            "n": "monkey",
+            "un": "\u{1F412}"
+        },
+        {
+            "n": "chicken",
+            "un": "\u{1F414}"
+        },
+        {
+            "n": "elephant",
+            "un": "\u{1F418}"
+        },
+        {
+            "n": "octopus",
+            "un": "\u{1F419}"
+        },
+        {
+            "n": "bee",
+            "un": "\u{1F41D}"
+        },
+        {
+            "n": "fish",
+            "un": "\u{1F41F}"
+        },
+        {
+            "n": "turtle",
+            "un": "\u{1F422}"
+        },
+        {
+            "n": "penguin",
+            "un": "\u{1F427}"
+        },
+        {
+            "n": "cow",
+            "un": "\u{1F42E}"
+        },
+        {
+            "n": "dolphin",
+            "un": "\u{1F42C}"
+        },
+        {
+            "n": "mouse",
+            "un": "\u{1F42D}"
+        },
+        {
+            "n": "tiger",
+            "un": "\u{1F42F}"
+        },
+        {
+            "n": "rabbit",
+            "un": "\u{1F430}"
+        },
+        {
+            "n": "whale",
+            "un": "\u{1F433}"
+        },
+        {
+            "n": "dog",
+            "un": "\u{1F436}"
+        },
+        {
+            "n": "pig",
+            "un": "\u{1F437}"
+        },
+        {
+            "n": "frog",
+            "un": "\u{1F438}"
+        },
+        {
+            "n": "hamster",
+            "un": "\u{1F439}"
+        },
+        {
+            "n": "bear",
+            "un": "\u{1F43B}"
+        },
+        {
+            "n": "panda",
+            "un": "\u{1F43C}"
+        },
+        {
+            "n": "boy",
+            "un": "\u{1F466}"
+        },
+        {
+            "n": "girl",
+            "un": "\u{1F467}"
+        },
+        {
+            "n": "woman",
+            "un": "\u{1F469}"
+        },
+        {
+            "n": "man",
+            "un": "\u{1F468}"
+        },
+        {
+            "n": "ghost",
+            "un": "\u{1F47B}"
+        },
+        {
+            "n": "skull",
+            "un": "\u{1F480}"
+        },
+        {
+            "n": "broken heart",
+            "un": "\u{1F494}"
+        },
+        {
+            "n": "poo",
+            "un": "\u{1F4A9}"
+        },
+        {
+            "n": "skull",
+            "un": "\u{1F480}"
+        },
+        {
+            "n": "wine",
+            "un": "\u{1F377}"
+        },
+        {
+            "n": "beer",
+            "un": "\u{1F37A}"
+        },
+    ]
+};
+
+(function() {
+    var b = document.getElementsByTagName('button');
+    b[0].addEventListener('click', function() {
+        var newT = [];
+        var t = document.getElementById('convert-text');
+        var ct = document.getElementById('converted-text');
+        t = t.value;
+
+        // Split each word into array for easier traversing
+        var tArr = t.split(' ');
+        // Loop through text to convert
+        for (var i = 0, l = tArr.length; i < l; i++) {
+            // Loop through emoji names
+            for (var j = 0, l2 = ed.data.length; j < l2; j++) {
+                if (tArr[i].indexOf(ed.data[j].n) !== -1) {
+                    // Replace instance of emoji name in string without
+                    // changing any punctuation, etc
+                    newT.push(tArr[i].replace(ed.data[j].n, ed.data[j].un));
+                    break;
+                }
+                else {
+                    // If it's the end of the array of emoji, push the unmatched text
+                    if (j === ed.data.length - 1) {
+                        newT.push(tArr[i]);
+                    }
+                }
+            }
+        }
+        ct.value = " ";
+        ct.value = newT.join(' ');
+    });
+}());
